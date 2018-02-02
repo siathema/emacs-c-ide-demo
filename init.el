d20ddc009123f100c50d3ad3c0a26ed39c14a9cd
Matthias Linford <nate.holaday@gmail.com>
2018-02-02 15:07:33 -0800

    updating settings

--- init.el
+++ init.el
@@ -36,12 +36,44 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("eea01f540a0f3bc7c755410ea146943688c4e29bea74a29568635670ab22f9bc" default)))
  '(package-selected-packages
    (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+    (monokai-alt-theme zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;my customizations
+(global-linum-mode t)
+(load-theme 'monokai-alt)
+(visual-line-mode t)
+
+;; change build commad to Ninja
+(setq compile-command "ninja")
+
+;; CMake-mode
+(setq load-path (cons (expand-file-name "/dir/with/cmake-mode") load-path))
+(require 'cmake-mode)
+
+; stolen from Casey Muratori
+; Bright-red TODOs
+(setq fixme-modes '(c++-mode c-mode emacs-lisp-mode))
+(make-face 'font-lock-fixme-face)
+(make-face 'font-lock-note-face)
+(make-face 'font-lock-caution-face)
+(mapc (lambda (mode)
+        (font-lock-add-keywords
+         mode
+         '(("\\<\\(TODO\\)" 1 'font-lock-fixme-face t)
+           ("\\<\\(NOTE\\)" 1 'font-lock-note-face t)
+           ("\\<\\(CAUTION\\)" 1 'font-lock-caution-face t))))
+      fixme-modes)
+(modify-face 'font-lock-fixme-face "Red" nil nil t nil t nil nil)
+(modify-face 'font-lock-note-face "Dark Green" nil nil t nil t nil nil)
+(modify-face 'font-lock-caution-face "deep sky blue" nil nil t nil t nil nil)
